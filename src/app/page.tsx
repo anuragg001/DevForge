57ad9876f81cd01e45a26a107d56683dbce8ef12
anurag jaiswal <anuraggjaiswal@gmail.com>
2025-03-02 21:19:57 +0530

    Intial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import { SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs"
 import Image from "next/image";
 
 export default function Home() {
-  console.log("hello from server side")
   return (
     <div>
-  hello
+    
     </div>
   );
 }
